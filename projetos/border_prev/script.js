8a5cdbbe2e7c1197be26dad0f0232713d96fa534
karlosmafra <kadumaframoraes@gmail.com>
2023-04-16 15:49:59 -0300

    border prev

--- projetos/border_prev/script.js
+++ projetos/border_prev/script.js
@@ -1,5 +1,6 @@
 function visualizar() {
     var div = document.querySelector('div#div')
+    var code = document.querySelector('div#code')
 
     var tl = Number(document.querySelector('input#tl').value)
     var tr = Number(document.querySelector('input#tr').value)
@@ -10,4 +11,9 @@ function visualizar() {
     div.style.borderTopRightRadius = `${tr}px`
     div.style.borderBottomLeftRadius = `${bl}px`
     div.style.borderBottomRightRadius = `${br}px`
+
+    code.innerHTML = `border-top-left-radius: ${tl}px; <br>
+    border-top-right-radius: ${tr}px; <br>
+    border-bottom-right-radius: ${br}px; <br>
+    border-bottom-left-radius: ${bl}px;`
 }
